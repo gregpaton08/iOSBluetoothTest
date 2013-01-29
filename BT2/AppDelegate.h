7076b9b9c797bb71fb41bbc578aa0e034cb6c9ed
gregpaton08 <gregpaton08@gmail.com>
2013-01-29 12:50:54 -0500

    Initial Commit

---
+++ BT2/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  BT2
+//
+//  Created by Greg Paton on 1/29/13.
+//  Copyright (c) 2013 Greg Paton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ BT2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BT2
+//
+//  Created by Greg Paton on 1/29/13.
+//  Copyright (c) 2013 Greg Paton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
